f2d3382d4b6eaeb30d268a2acfce0f4dcc74222a
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-05-10 13:09:39 +0200

    Add project files.

---
+++ Controllers/PizzeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzeriaWebApp.Models;
+using PizzeriaWebApp.Utils;
+
+namespace PizzeriaWebApp.Controllers
+{
+    public class PizzeController : Controller
+    {
+        [HttpGet]
+        public IActionResult LeMiePizze()
+        {
+            List<Pizza> pizzas = PizzaData.GetPizza();
+            return View(pizzas);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+            return View();
+
+        }
+    }
+
+    
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,30 @@
+﻿namespace PizzeriaWebApp.Models
+{
+    public class Pizza
+    {
+
+        
+        public string nome { get; set; }
+        public double prezzo { get; set; }
+        public string descrizione { get; set; }
+        public string foto { get; set; }
+
+        public Pizza()
+        {
+
+
+
+        }
+
+        public Pizza(string nome , double prezzo , string descrizione , string foto)
+        {
+
+            this.nome = nome;
+            this.prezzo = prezzo;
+            this.descrizione = descrizione;
+            this.foto = foto;
+
+        }
+
+    }
+}
